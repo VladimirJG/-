794a89b78421ed32c0a7f6f229994c97172039f5
VladimirJG <vladimi85@gmail.com>
2022-05-02 17:55:23 +0300

    MOY1

---
+++ Samost.js
@@ -0,0 +1,20 @@
+// Рисуем столько котиков сколько захотим
+/* var drawCats = function(HowManyTimes) {
+    for (var i = 0; i < HowManyTimes; i++) {
+        console.log(i + " =^.^=");
+    }
+}
+drawCats(7) */
+
+/* var users = function(HowManyUsers) {
+    let a = 15
+    for (var i = 0; i < HowManyUsers; i++) {
+        console.log(i + a + " =^.^=");
+    }
+}
+users(5) */
+
+/* var secondInMinute = 60
+var minuteInHour = 60
+var secondsInHour = secondInMinute * minuteInHour
+console.log(secondsInHour) */
