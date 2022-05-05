c1950331ce9de55678705f5632b13cd5a2cbd23c
VladimirJG <vladimi85@gmail.com>
2022-05-05 18:29:44 +0300

    new task 05.05.(2)

--- Samost.js
+++ Samost.js
@@ -14,7 +14,7 @@ drawCats(7) */
 }
 users(5) */
 
-var secondInMinute = 60
+/* var secondInMinute = 60
 var minuteInHour = 60
 var secondsInHour = secondInMinute * minuteInHour
 console.log(secondsInHour)
@@ -26,4 +26,24 @@ var secondInYear = secondsInDay * daysInYear
 console.log(secondInYear)
 var age = 36
 var secondsOfMe = age * secondInYear
-console.log(secondsOfMe)
+console.log(secondsOfMe) */
+
+/* var higfFives = 0
+    ++higfFives */
+var x = 10
+x = x + 5
+console.log(x)
+
+var score = 10
+score += 7
+console.log(score)
+score -= 3
+console.log(score)
+
+var balloons = 100
+balloons *= 2
+console.log(balloons)
+
+var balloons = 100
+balloons /= 4
+console.log(balloons)
